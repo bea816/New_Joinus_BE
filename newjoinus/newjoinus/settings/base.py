4c6a4ce204b826179e800621242b7aa66d9e3894
bea816 <kimnadon@naver.com>
2025-03-27 15:36:44 +0900

    장고 세팅 분리 및 배포 준비

--- newjoinus/newjoinus/settings/base.py
+++ newjoinus/newjoinus/settings/base.py
@@ -11,21 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-o5)f6qwf_nlpw!yc_fecyqpacdks$=a#ar7(d@a6o2a!a3ovpo'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+BASE_DIR = Path(__file__).resolve().parent.parent.parent
 
 
 # Application definition
@@ -40,11 +29,13 @@ INSTALLED_APPS = [
 
     # 앱 제외
     'rest_framework',
-    
+    'corsheaders', # 배포
+
     # 앱
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,16 +65,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'newjoinus.wsgi.application'
 
-
-# Database
-# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
