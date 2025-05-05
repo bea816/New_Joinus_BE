9ab3db0733a58cc70b4951bdf1d133ec6d59dfe3
bea816 <kimnadon@naver.com>
2025-05-06 01:21:28 +0900

    crontab test

--- newjoinus/settings/prod.py
+++ newjoinus/settings/prod.py
@@ -1,6 +1,5 @@
 from .base import *
 import environ
-from django.core.exceptions import ImproperlyConfigured
 
 def get_env_variable(var_name):
     try:
@@ -9,16 +8,14 @@ def get_env_variable(var_name):
         error_msg = 'Set the {} environment variable'.format(var_name)
         raise ImproperlyConfigured(error_msg)
 
-def parse_env_list(var_name):
-    return [item.strip() for item in get_env_variable(var_name).split(',') if item.strip()]
-
 SECRET_KEY = get_env_variable('DJANGO_SECRET')
-DEBUG = get_env_variable('DJANGO_DEBUG') == 'True'
 
 # 배포
-ALLOWED_HOSTS = parse_env_list('DJANGO_ALLOWED_HOSTS')
-CSRF_TRUSTED_ORIGINS = parse_env_list('DJANGO_CSRF_TRUSTED_ORIGINS')
-CORS_ALLOWED_ORIGINS = parse_env_list('DJANGO_CORS_ALLOWED_ORIGINS')
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", ".cloudtype.app"]
+CSRF_TRUSTED_ORIGINS = ['https://*.cloudtype.app']
+
+
+DEBUG = True
 
 DBNAME = get_env_variable('DBNAME')
 DBUSER = get_env_variable('DBUSER')
